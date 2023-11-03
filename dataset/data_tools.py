7ac9c3bf08c3718bb8afb12932b30fe5151b4ffe
fireball0213 <1306189897@qq.com>
2023-11-03 21:07:42 +0800

    refactor

---
+++ dataset/data_tools.py
@@ -0,0 +1,101 @@
+
+# -*- coding: UTF-8 -*- #
+"""
+@filename:data_tools.py
+@author:201300086
+@time:2023-03-13
+"""
+import numpy as np
+import torch
+import datetime
+
+
+def time_features(dates, timeenc=1, freq='h'):
+    """
+    > `time_features` takes in a `dates` dataframe with a 'dates' column and extracts the date down to `freq` where freq can be any of the following if `timeenc` is 0:
+    > * m - [month]
+    > * w - [month]
+    > * d - [month, day, weekday]
+    > * b - [month, day, weekday]
+    > * h - [month, day, weekday, hour]
+    > * t - [month, day, weekday, hour, *minute]
+    >
+    > If `timeenc` is 1, a similar, but different list of `freq` values are supported (all encoded between [-0.5 and 0.5]):
+    > * Q - [month]
+    > * M - [month]
+    > * W - [Day of month, week of year]
+    > * D - [Day of week, day of month, day of year]
+    > * B - [Day of week, day of month, day of year]
+    > * H - [Hour of day, day of week, day of month, day of year]
+    > * T - [Minute of hour*, hour of day, day of week, day of month, day of year]
+    > * S - [Second of minute, minute of hour, hour of day, day of week, day of month, day of year]
+
+    *minute returns a number from 0-3 corresponding to the 15 minute period it falls into.
+    """
+    if timeenc == 0:  # 如果timeenc为0，根据日期提取特定的时间特征
+        dates['month'] = dates.date.apply(lambda row: row.month, 1)  # 提取月份
+        dates['day'] = dates.date.apply(lambda row: row.day, 1)  # 提取日期
+        dates['weekday'] = dates.date.apply(lambda row: row.weekday(), 1)  # 提取星期几
+        dates['hour'] = dates.date.apply(lambda row: row.hour, 1)  # 提取小时
+        dates['minute'] = dates.date.apply(lambda row: row.minute, 1)  # 提取分钟
+        dates['minute'] = dates.minute.map(lambda x: x // 15)  # 将分钟映射到0-3表示15分钟的一个区间
+        freq_map = {
+            'y': [], 'm': ['month'], 'w': ['month'], 'd': ['month', 'day', 'weekday'],
+            'b': ['month', 'day', 'weekday'], 'h': ['month', 'day', 'weekday', 'hour'],
+            't': ['month', 'day', 'weekday', 'hour', 'minute'],
+        }
+        return dates[freq_map[freq.lower()]].values  # 返回对应频率的时间特征值
+    # if timeenc==1:
+    #     dates = pd.to_datetime(dates.date.values)
+    #     return np.vstack([feat(dates) for feat in time_features_from_frequency_str(freq)]).transpose(1,0)
+
+
+# 定义一个标准化类
+class StandardScaler():
+    def __init__(self):
+        self.mean = 0.
+        self.std = 1.
+
+    def fit(self, data):
+        self.mean = data.mean(0)
+        self.std = data.std(0)
+
+    # 将均值和标准差转换为与数据类型和设备相同的tensor
+    def transform(self, data):
+        mean = torch.from_numpy(self.mean).type_as(data).to(data.device) if torch.is_tensor(data) else self.mean
+        std = torch.from_numpy(self.std).type_as(data).to(data.device) if torch.is_tensor(data) else self.std
+        return (data - mean) / std
+
+    # 与transform相反，将标准化后的数据恢复到原始范围
+    def inverse_transform(self, data):
+        mean = torch.from_numpy(self.mean).type_as(data).to(data.device) if torch.is_tensor(data) else self.mean
+        std = torch.from_numpy(self.std).type_as(data).to(data.device) if torch.is_tensor(data) else self.std
+        if data.shape[-1] != mean.shape[-1]:
+            mean = mean[-1:]
+            std = std[-1:]
+        return (data * std) + mean
+
+
+def get_one_hot(index, size):
+    '''
+    获得一个one-hot的编码
+    index:编码值
+    size:编码长度
+    '''
+    one_hot = [0 for _ in range(1, size + 1)]  #np.zeros([size], dtype=int)#
+    one_hot[index - 1] = 1
+    return one_hot
+
+
+# 定义一个函数用于获得数据的one-hot特征
+def get_one_hot_feature(data_stamp, freq):
+    if freq == "h":  # 如果频率为小时
+        data_one_hot = np.hstack((data_stamp[:, :1], data_stamp[:, 2:3] + 1))  # 处理数据获取one-hot编码需要的部分
+        func_ = np.frompyfunc(get_one_hot, 2, 1)  # 使用numpy的frompyfunc函数创建one-hot转换函数
+        data_one_hot = func_(data_one_hot, [12, 7])  # 应用转换函数获取one-hot编码
+        data_one_hot=data_one_hot[:,0]+data_one_hot[:,1]  # 合并one-hot编码结果
+        data_one_hot=np.array(data_one_hot.tolist())  # 将one-hot编码结果转换为numpy数组
+        return data_one_hot  # 返回one-hot编码数组
+    else:
+        print("unsupported freq!")  # 如果传入的频率不支持，则打印错误信息
+        return 0
