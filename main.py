4db7d2043e9ed195d0706de3856eb69bdb472ecf
fireball0213 <1306189897@qq.com>
2023-12-06 22:12:14 +0800

    add LSH and recursive(both useless)

--- main.py
+++ main.py
@@ -14,21 +14,24 @@ def get_args():
     parser = argparse.ArgumentParser()
 
     # dataset config
-    parser.add_argument('--data_path', type=str, default='./dataset/ETT/ETTh1.csv')
-    # parser.add_argument('--data_path', type=str, default='./dataset/ETT/ETTh2.csv')
-    # parser.add_argument('--data_path', type=str, default='./dataset/ETT/ETTm1.csv')
-    # parser.add_argument('--data_path', type=str, default='./dataset/electricity/electricity.csv')
-    # parser.add_argument('--data_path', type=str, default='./dataset/exchange_rate/exchange_rate.csv')
-    # parser.add_argument('--data_path', type=str, default='./dataset/illness/national_illness.csv')
-    # parser.add_argument('--data_path', type=str, default='./dataset/traffic/traffic.csv')
-    # parser.add_argument('--data_path', type=str, default='./dataset/weather/weather.csv')
-
     parser.add_argument('--train_data_path', type=str, default='./dataset/m4/Daily-train.csv')
     parser.add_argument('--test_data_path', type=str, default='./dataset/m4/Daily-test.csv')
 
+    # parser.add_argument('--data_path', type=str, default='./dataset/ETT/ETTh1.csv')
+    # parser.add_argument('--data_path', type=str, default='./dataset/ETT/ETTh2.csv')
+    # parser.add_argument('--data_path', type=str, default='./dataset/ETT/ETTm1.csv')
+    parser.add_argument('--data_path', type=str, default='./dataset/illness/national_illness.csv')
+    # parser.add_argument('--data_path', type=str, default='./dataset/electricity/electricity.csv')
+    # parser.add_argument('--data_path', type=str, default='./dataset/exchange_rate/exchange_rate.csv')
+    # parser.add_argument('--data_path', type=str, default='./dataset/traffic/traffic.csv')
+    # parser.add_argument('--data_path', type=str, default='./dataset/weather/weather.csv')
+
+    # parser.add_argument('--period', type=int, default=24, help='period used in TsfKNN and MASE,ETT:24,illness:52')
+    parser.add_argument('--period', type=int, default=52, help='period used in TsfKNN and MASE,ETT:24,illness:52')
+
     # parser.add_argument('--dataset', type=str, default='M4', help='dataset type, options: [M4, ETT, Custom]')
-    parser.add_argument('--dataset', type=str, default='ETT', help='dataset type, options: [M4, ETT, Custom]')
-    # parser.add_argument('--dataset', type=str, default='Custom', help='dataset type, options: [M4, ETT, Custom]')
+    # parser.add_argument('--dataset', type=str, default='ETT', help='dataset type, options: [M4, ETT, Custom]')
+    parser.add_argument('--dataset', type=str, default='Custom', help='dataset type, options: [M4, ETT, Custom]')
 
     parser.add_argument('--target', type=str, default='OT', help='target feature')
     parser.add_argument('--ratio_train', type=int, default=0.6, help='train dataset length')
@@ -40,41 +43,42 @@ def get_args():
     parser.add_argument('--seq_len', type=int, default=96, help='input sequence length')
     parser.add_argument('--pred_len', type=int, default=32, help='prediction sequence length')
 
+
     # model define
     # parser.add_argument('--model', type=str, default='MeanForecast', help='model name')#, required=True
     # parser.add_argument('--model', type=str, default='LastValueForecast', help='model name')#, required=True
-    parser.add_argument('--model', type=str, default='Autoregression', help='model name')#, required=True
+    # parser.add_argument('--model', type=str, default='Autoregression', help='model name')#, required=True
     # parser.add_argument('--model', type=str, default='ExponentialMovingAverage', help='model name')#, required=True
     # parser.add_argument('--model', type=str, default='DoubleExponentialSmoothing', help='model name')#, required=True
-    # parser.add_argument('--model', type=str, default='TsfKNN', help='model name')#, required=True
+    parser.add_argument('--model', type=str, default='TsfKNN', help='model name')#, required=True
 
     # EMA define
     parser.add_argument('--alpha', type=float, default=0.9, help='alpha used in ExponentialMovingAverage')
-    parser.add_argument('--beta', type=float, default=0.2, help='beta used in DoubleExponentialSmoothing')
+    parser.add_argument('--beta', type=float, default=0.1, help='beta used in DoubleExponentialSmoothing')
 
     # multi-step ahead strategy used in LR and TsfKNN,多步直接预测or单步迭代预测
     parser.add_argument('--msas', type=str, default='MIMO', help=' options: [MIMO, recursive]')
     # parser.add_argument('--msas', type=str, default='recursive', help='options: [MIMO, recursive]')
 
-
     #TsfKNN define
-    parser.add_argument('--n_neighbors', type=int, default=5, help='number of neighbors used in TsfKNN')
-    parser.add_argument('--distance', type=str, default='euclidean', help='distance used in TsfKNN')
+    parser.add_argument('--n_neighbors', type=int, default=9, help='number of neighbors used in TsfKNN')
+    # parser.add_argument('--distance', type=str, default='euclidean', help='distance used in TsfKNN')
     # parser.add_argument('--distance', type=str, default='manhattan', help='distance used in TsfKNN')
-    # parser.add_argument('--distance', type=str, default='chebyshev', help='distance used in TsfKNN')
+    parser.add_argument('--distance', type=str, default='chebyshev', help='distance used in TsfKNN')
     parser.add_argument('--decompose', type=bool, default=True, help='stl_modified distance used in TsfKNN')
     # parser.add_argument('--decompose', type=bool, default=False, help='stl_modified distance used in TsfKNN')
-    # parser.add_argument('--period', type=int, default=24, help='period used in TsfKNN,ETT:24,illness:52')
-    parser.add_argument('--period', type=int, default=52, help='period used in TsfKNN,ETT:24,illness:52')
+
     parser.add_argument('--approximate_knn', type=bool, default=False, help='approximate_knn used in TsfKNN')
     # parser.add_argument('--approximate_knn', type=bool, default=True, help='approximate_knn used in TsfKNN')
+    parser.add_argument('--hash_size', type=int, default=10, help='hash_num used in LSH')
+
 
     # transform define
     # parser.add_argument('--transform', type=str, default='IdentityTransform')
-    # parser.add_argument('--transform', type=str, default='Normalization')
+    parser.add_argument('--transform', type=str, default='Normalization')
     # parser.add_argument('--transform', type=str, default='Standardization')
     # parser.add_argument('--transform', type=str, default='MeanNormalization')
-    parser.add_argument('--transform', type=str, default='BoxCox')
+    # parser.add_argument('--transform', type=str, default='BoxCox')
 
 
     args = parser.parse_args()
@@ -115,7 +119,7 @@ if __name__ == '__main__':
     # load dataset
     dataset = get_dataset(args)
 
-    data_visualize(dataset, 500)
+    # data_visualize(dataset, 500)
     plt.show()
 
 
@@ -125,7 +129,7 @@ if __name__ == '__main__':
     # data transform
     transform = get_transform(args)
     # create trainer
-    trainer = MLTrainer(model=model, transform=transform, dataset=dataset)
+    trainer = MLTrainer(args,model=model, transform=transform, dataset=dataset)
     # # train model
     trainer.train()
     print("train finished")
