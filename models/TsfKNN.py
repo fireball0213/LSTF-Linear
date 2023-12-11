4e172c021723a659c15c9665931d619ade8a8485
fireball0213 <1306189897@qq.com>
2023-12-11 15:55:54 +0800

    增加马氏、带权欧拉distance函数，用于非STL的search

--- models/TsfKNN.py
+++ models/TsfKNN.py
@@ -2,7 +2,7 @@ import numpy as np
 from numpy.lib.stride_tricks import sliding_window_view
 from statsmodels.tsa.seasonal import STL
 from models.base import MLForecastModel
-from utils.distance import euclidean, manhattan, chebyshev
+from utils.distance import euclidean, manhattan, chebyshev,get_distance,preprocess_ts2,preprocess_inv
 from tqdm import tqdm
 from dataset.data_visualizer import plot_STL
 from sklearn.linear_model import LinearRegression
@@ -18,58 +18,80 @@ class TsfKNN(MLForecastModel):
         self.seq_len = args.seq_len
         self.pred_len = args.pred_len
         self.msas = args.msas
-        if args.distance == 'euclidean':
-            self.distance = euclidean
-        elif args.distance == 'manhattan':
-            self.distance = manhattan
-        elif args.distance == 'chebyshev':
-            self.distance = chebyshev
+        self.dia_func= args.distance
+        self.distance = get_distance(args)
         self.decompose = args.decompose  # 是否考虑趋势和季节性
         self.period = args.period  # 季节性的值
-        self.trend = args.trend
+        self.trend = args.trend #趋势的预测方法
         self.approximate_knn = args.approximate_knn  # 是否使用近似knn
         self.hash_size = args.hash_size
+        self.distance_dim = args.distance_dim  # 是否是多变量距离
+        self.weighted = args.weighted
         super().__init__()
 
     def _fit(self, X: np.ndarray) -> None:
-        self.X = X[0, :, -1]
-        self.X_slide = sliding_window_view(self.X, self.seq_len + self.pred_len)
-        if self.decompose:
-            self.X_stl = STL(self.X, period=self.period).fit()  # 对整个序列进行STL分解
-            # plot_STL(self.X_stl,400)
-            subseries = sliding_window_view(self.X_stl.trend, self.seq_len + self.pred_len)
-            self.trend_model = LinearRegression()
-            if self.trend == 'AR':
-                trend_X = subseries[:, :self.seq_len]
-                trend_y = subseries[:, self.seq_len:]
-                self.trend_model.fit(trend_X, trend_y)
-        if self.approximate_knn:
-            self.lsh_model = LSHash(hash_size=self.hash_size, input_dim=self.seq_len)
-            for i, d in enumerate(self.X_slide):
-                point = d[:self.seq_len]
-                self.lsh_model.index(point, extra_data=i)
+        if self.distance_dim == 'OT':
+            self.X = X[0, :, -1]
+            self.X_slide = sliding_window_view(self.X, self.seq_len + self.pred_len)
+            self.X_slide_seq = self.X_slide[:, :self.seq_len]
+            if self.decompose:
+                self.X_stl = STL(self.X, period=self.period).fit()  # 对整个序列进行STL分解
+                plot_STL(self.X_stl,1000)
+                subseries = sliding_window_view(self.X_stl.trend, self.seq_len + self.pred_len)
+                self.trend_model = LinearRegression()
+                if self.trend == 'AR':
+                    trend_X = subseries[:, :self.seq_len]
+                    trend_y = subseries[:, self.seq_len:]
+                    self.trend_model.fit(trend_X, trend_y)
+            if self.approximate_knn:
+                self.lsh_model = LSHash(hash_size=self.hash_size, input_dim=self.seq_len)
+                for i, d in enumerate(self.X_slide):
+                    point = d[:self.seq_len]
+                    self.lsh_model.index(point, extra_data=i)
+        elif self.distance_dim=='multi':
+            self.X = X[0, :, :]
+            self.X_slide = sliding_window_view(self.X, (self.seq_len + self.pred_len,1))
+            self.X_slide = self.X_slide.transpose(0,2,1,3)[:,:,:,0]
+            self.X_slide_seq = self.X_slide[:, :self.seq_len]
+            # 计算协方差矩阵的逆，用于加速，只计算一次
+            self.inv_covmat = preprocess_inv(self.X)#使用sliding前数据，更合理
+            # self.inv_covmat = preprocess_ts2(self.X_slide_seq)
 
-    def _search(self, x, X_slide, seq_len, pred_len):
+    def _search(self, x, pred_len):
         # 找到训练集中与x最相似的k个时间序列，然后对这k个时间序列的后pred_len个值求均值，作为预测值
         if self.approximate_knn:
             result = self.lsh_model.query(x.ravel(), num_results=self.k)
             indices_of_smallest_k = [res[0][1] for res in result]
         else:
-            distances = self.distance(x, X_slide[:, :seq_len])
+            if self.dia_func == 'mahalanobis':
+                distances = self.distance(x, self.X_slide_seq, self.inv_covmat)
+            elif self.dia_func == 'weighted_euclidean':
+                weights = np.array(self.weighted)
+                '''
+                权重稀疏时，可以加速计算
+                '''
+                nonzero_weights_indices = weights != 0
+                weights = weights[nonzero_weights_indices]
+                distances = self.distance(x[..., nonzero_weights_indices], self.X_slide_seq[..., nonzero_weights_indices],weights=weights)
+            else:
+                distances = self.distance(x, self.X_slide_seq)
             indices_of_smallest_k = np.argsort(distances)[:self.k]
 
         if self.msas == 'MIMO':
-            neighbor_fore = X_slide[indices_of_smallest_k, seq_len:]  # 获取这k个最近邻时间序列的预测部分
+            if self.distance_dim == 'OT':
+                neighbor_fore = self.X_slide[indices_of_smallest_k, self.seq_len:]  # 获取这k个最近邻时间序列的预测部分
+            else:
+                neighbor_fore = self.X_slide[indices_of_smallest_k, self.seq_len:,-1]
             x_fore = np.mean(neighbor_fore, axis=0, keepdims=True)  # 计算这些最近邻时间序列预测的平均值
             return x_fore
         elif self.msas == 'recursive':
-            neighbor_fore = X_slide[indices_of_smallest_k, seq_len].reshape((-1, 1))  # 获取这k个最近邻时间序列的下一个时间点的值
+            neighbor_fore = self.X_slide[indices_of_smallest_k, self.seq_len].reshape((-1, 1))  # 获取这k个最近邻时间序列的下一个时间点的值
             x_fore = np.mean(neighbor_fore, axis=0, keepdims=True)  # 计算这些最近邻时间序列下一个时间点的平均值
             x_new = np.concatenate((x[:, 1:], x_fore), axis=1)  # 更新x，准备下一次递归预测
             if pred_len == 1:
                 return x_fore
             else:
-                return np.concatenate((x_fore, self._search(x_new, X_slide, seq_len, pred_len - 1)), axis=1)
+                return np.concatenate((x_fore, self._search(x_new,  pred_len - 1)), axis=1)
 
     def STL_search(self,x_stl_origin, x_stl_trend,x_stl_seasonal , x_stl_resid, seq_len, pred_len):
         #在STL分解后的序列上进行搜索
@@ -124,9 +146,8 @@ class TsfKNN(MLForecastModel):
         fore = []
         for x in tqdm(X):
             x = np.expand_dims(x, axis=0)
-            x_stl = STL(x[0], period=self.period).fit()
-
             if self.decompose:
+                x_stl = STL(x[0], period=self.period).fit()
                 # 传入STL分解后的序列进行搜索
                 x_fore = self.STL_search(x_stl.observed,x_stl.trend,x_stl.seasonal,x_stl.resid, self.seq_len, pred_len)
                 if self.trend == 'AR':
@@ -137,8 +158,9 @@ class TsfKNN(MLForecastModel):
                     x_stl_trend = self.trend_model.predict(np.arange(self.seq_len, pred_len + self.seq_len).reshape((-1, 1)))
                     x_fore += x_stl_trend.ravel()
             else:
-                x_fore = self._search(x, self.X_slide, self.seq_len, pred_len)
+                x_fore = self._search(x,  pred_len)
             fore.append(x_fore)
 
         fore = np.array(fore).reshape((-1, pred_len))
+
         return fore
