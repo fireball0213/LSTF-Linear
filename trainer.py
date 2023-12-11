8deb4b8776f12fae3ce2cc8795dfdde727652991
fireball0213 <1306189897@qq.com>
2023-12-11 15:39:18 +0800

    fixbug 多变量test_Y

--- trainer.py
+++ trainer.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numpy.lib.stride_tricks import sliding_window_view
 from utils.metrics import mse, mae, mape, smape, mase
-
+from dataset.data_visualizer import data_vi
 
 class MLTrainer:
     def __init__(self, args,model, transform, dataset):
@@ -9,10 +9,12 @@ class MLTrainer:
         self.transform = transform
         self.dataset = dataset
         self.period = args.period
+        self.distance_dim = args.distance_dim
 
     def train(self):
         train_X = self.dataset.train_data
         t_X = self.transform.transform(train_X,update=True)
+        # data_vi(t_X, 2000)
         self.model.fit(t_X)
 
 
@@ -27,16 +29,22 @@ class MLTrainer:
         fore = self.transform.inverse_transform(fore)
         return fore
 
-    def evaluate(self, dataset, seq_len=96, pred_len=32):
+    def evaluate(self,dataset, seq_len=96, pred_len=32):
         if dataset.type == 'm4':
             test_X = dataset.train_data
             test_Y = dataset.test_data
             pred_len = dataset.test_data.shape[-1]
         else:
-            test_data = dataset.test_data[:, :, -1]
-            subseries = np.concatenate(([sliding_window_view(v, seq_len + pred_len) for v in test_data]))
-            test_X = subseries[:, :seq_len]
-            test_Y = subseries[:, seq_len:]
+            if self.distance_dim=='OT':
+                test_data = dataset.test_data[:, :, -1]
+                subseries = np.concatenate(([sliding_window_view(v, seq_len + pred_len) for v in test_data]))
+                test_X = subseries[:, :seq_len]
+                test_Y = subseries[:, seq_len:]
+            elif self.distance_dim=='multi':
+                subseries = np.concatenate(([sliding_window_view(v, (seq_len + pred_len,1)) for v in dataset.test_data]))
+                subseries = subseries.transpose(0,2,1,3)[:,:,:,0]
+                test_X = subseries[:, :seq_len,:]
+                test_Y = subseries[:, seq_len:,-1]
         test_X = self.transform.transform(test_X)
         fore = self.model.forecast(test_X, pred_len=pred_len)
         # test_Y = self.transform.transform(test_Y)
